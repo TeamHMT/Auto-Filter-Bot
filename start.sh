7744e16da9df3368788ecb0114df13f0e4f41a4c
Kgashok06 <121030620+Kgashok06@users.noreply.github.com>
2022-12-21 21:17:55 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/CrazyDeveloperTG/Doctor-Strange.git /Doctor-Strange
+  git clone https://github.com/Kgashok06/Auto-filter-bot.git /Auto-filter-bot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Doctor-Strange
