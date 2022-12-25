40c82194f23e2938937d590f8689b0278a9d84f7
TeamHMT <121344166+TeamHMT@users.noreply.github.com>
2022-12-25 17:59:49 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/TeamHMT/Auto-Filter-Bot.git /Auto-filter-bot
+  git clone https://github.com/TeamHMT/Auto-Filter-Bot.git /Auto-Filter-Bot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Auto-filter-bot
+  git clone $UPSTREAM_REPO /Auto-Filter-Bot
 fi
-cd /Auto-filter-bot
+cd /Auto-Filter-Bot
 pip3 install -U -r requirements.txt
 echo "𝙎𝙩𝙖𝙧𝙩𝙞𝙣𝙜....🍁"
 python3 bot.py
